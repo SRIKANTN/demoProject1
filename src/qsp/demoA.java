d60cc2804a0610c371b25acd816c3118d4246c44
VinayN123 <vinayn348@gmail.com>
2019-11-11 15:34:26 +0530

    cghgfhjgjj

--- src/qsp/demoA.java
+++ src/qsp/demoA.java
@@ -5,7 +5,7 @@ public class demoA
 	public static void main(String[] args) 
 	{
 		System.out.println("hiii");
-		
+		System.out.println("hi");
 	}
 
 }
---
+++ src/qspA/DemoA.java
@@ -0,0 +1,9 @@
+package qspA;
+
+public class DemoA
+{
+	public static void main(String[] args) {
+		System.out.println("hiii.........");
+	}
+
+}
